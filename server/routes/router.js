8057ad65632012d5ef7546073e50f88bfc055a07
numannaeem <numan.naeem9@gmail.com>
2021-06-15 16:44:12 +0530

    logout, navbar added:

--- server/routes/router.js
+++ server/routes/router.js
@@ -4,12 +4,13 @@ const { Delete } = require("../controllers/deleteController");
 const { Edit } = require("../controllers/editController");
 const { getData } = require("../controllers/getDataController");
 const { getUser } = require("../controllers/getUserController");
+const { Logout } = require("../controllers/logoutController");
 const { Register } = require("../controllers/registerController");
 
 const router = express.Router();
 
 router.get('/',(req,res) => {
-    return res.end("sameSite: 'none', cookieparser not used")
+    return res.end("To-do app by Numan Naeem")
 })
 
 router.get('/items',getData);
@@ -18,6 +19,7 @@ router.post('/edit',Edit);
 router.post('/delete',Delete);
 router.post('/register',Register)
 router.get('/getUser',getUser)
+router.get('/logout',Logout)
 // router.post('/login',Login)
 
 
