8432a8a31b252c40ffa21f0c8590913ad40d1070
numannaeem <numan.naeem9@gmail.com>
2021-06-16 23:09:19 +0530

    Added Procfile

--- controllers/getUserController.js
+++
@@ -1,8 +0,0 @@
-const getUser = (req,res) => {
-    console.log(req.user)
-    if(req.user)
-        return res.json(req.user.username)
-    return res.status(404).send({message:'No user logged in'})
-}
-
-module.exports = { getUser }
